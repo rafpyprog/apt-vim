48410cd94dfad3efa9cf4dbb5e02baca7e0121d5
rafpyprog <rafael.alves.ribeiro@gmail.com>
2017-11-22 17:18:43 -0200

    test

---
+++ git-dist.bat
@@ -0,0 +1,9 @@
+git add .
+git commit -m "test"
+git push
+
+del C:\Users\05966258635\.vimrc
+rmdir /S /Q C:\Users\05966258635\.apt-vim
+rmdir /S /Q C:\Users\05966258635\.vim
+rmdir /S /Q C:\Users\05966258635\.vimpkg
+rmdir /S /Q C:\Users\05966258635\apt-vim
