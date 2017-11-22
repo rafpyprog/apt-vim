03dc74f45eb556fe961f0ce14457afa8a74ecbba
rafpyprog <rafael.alves.ribeiro@gmail.com>
2017-11-22 17:19:38 -0200

    test

--- test/get_install.bat
+++ test/get_install.bat
@@ -1,12 +1,4 @@
 @echo '---------------------------------------------'
-@echo ' Clean installation'
-@echo '---------------------------------------------'
-del C:\Users\05966258635\.vimrc
-rmdir /S /Q C:\Users\05966258635\.apt-vim
-rmdir /S /Q C:\Users\05966258635\.vim
-rmdir /S /Q C:\Users\05966258635\.vimpkg
-
-@echo '---------------------------------------------'
 @echo ' Acquire Installation script from repository'
 @echo '---------------------------------------------'
 powershell -command "Invoke-WebRequest https://raw.githubusercontent.com/rafpyprog/apt-vim/master/install.bat -OutFile install.bat"
