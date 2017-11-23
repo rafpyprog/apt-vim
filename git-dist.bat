4a5adc5d6e91d847a74d540821371cb977145cd2
rafpyprog <rafael.alves.ribeiro@gmail.com>
2017-11-23 17:48:52 -0200

    Fix-1

--- git-dist.bat
+++ git-dist.bat
@@ -42,7 +42,7 @@ if exist %VIMPKG% rmdir /S /Q %VIMPKG%
 if exist %APTVIM% rmdir /S /Q %APTVIM%
 
 echo Instalando teste
-set install_script=https://raw.githubusercontent.com/rafpyprog/apt-vim/master/install.bat -OutFile D:\mono-repo\apt-vim-fork\test\install.bat
+set install_script=https://raw.githubusercontent.com/rafpyprog/apt-vim/master/install.ps1 -OutFile D:\mono-repo\apt-vim-fork\test\install.ps1
 powershell -command "Invoke-WebRequest -DisableKeepAlive %install_script%"
 
-D:\mono-repo\apt-vim-fork\test\install.bat
+D:\mono-repo\apt-vim-fork\test\install.ps1
