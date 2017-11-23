b437e159d833d16389c1ea4fbc0db067b617600d
rafpyprog <rafael.alves.ribeiro@gmail.com>
2017-11-23 09:33:42 -0200

    test

--- git-dist.bat
+++ git-dist.bat
@@ -8,6 +8,7 @@ set VIM=C:\Users\05966258635\.vim
 set VIMPKG=C:\Users\05966258635\.vimpkg
 set APTVIM=C:\Users\05966258635\apt-vim
 
+@echo off
 if exist %VIMRC% del %VIMRC%
 if exist %APTVIM% rmdir /S /Q %APTVIM%
 if exist %VIM% rmdir /S /Q %VIM%
