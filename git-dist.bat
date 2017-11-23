3504b4ab59258d766491b45bdcfd3032ee7942d6
rafpyprog <rafael.alves.ribeiro@gmail.com>
2017-11-23 09:34:07 -0200

    test

--- git-dist.bat
+++ git-dist.bat
@@ -2,13 +2,13 @@ git add .
 git commit -m "test"
 git push
 
+@echo off
 set VIMRC=C:\Users\05966258635\.vimrc
 set APTVIM=C:\Users\05966258635\.apt-vim
 set VIM=C:\Users\05966258635\.vim
 set VIMPKG=C:\Users\05966258635\.vimpkg
 set APTVIM=C:\Users\05966258635\apt-vim
 
-@echo off
 if exist %VIMRC% del %VIMRC%
 if exist %APTVIM% rmdir /S /Q %APTVIM%
 if exist %VIM% rmdir /S /Q %VIM%
