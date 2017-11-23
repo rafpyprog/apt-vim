e0b903ac6fd81bcc93e4e5585ac060a5c9d1e726
rafpyprog <rafael.alves.ribeiro@gmail.com>
2017-11-23 17:47:18 -0200

    Fix

--- test/get_install.bat
+++ test/get_install.bat
@@ -2,5 +2,5 @@
 @echo ' Acquire Installation script from repository'
 @echo '---------------------------------------------'
 
-set install_script=https://raw.githubusercontent.com/rafpyprog/apt-vim/master/install.bat -OutFile install.bat
+set install_script=https://raw.githubusercontent.com/rafpyprog/apt-vim/master/install.ps1 -OutFile install.ps1
 powershell -command "Invoke-WebRequest -DisableKeepAlive %install_script%"
